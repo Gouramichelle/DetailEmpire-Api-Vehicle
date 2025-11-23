7f81e139ce408efaf78ec828012157e4c89f0689
Goura <gouramichelle24@gmail.com>
2025-11-23 19:35:28 -0300

    config

---
+++ src/test/java/com/detailempire/vehicle/VehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.detailempire.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
